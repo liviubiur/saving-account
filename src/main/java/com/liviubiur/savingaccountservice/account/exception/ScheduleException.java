3d290e2c864e2e9e5bf601b5e7027472176b32ec
Liviu <liviubiur@gmail.com>
2020-07-29 13:15:23 +0200

    Update ScheduleException.java

--- src/main/java/com/liviubiur/savingaccountservice/account/exception/ScheduleException.java
+++ src/main/java/com/liviubiur/savingaccountservice/account/exception/ScheduleException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ScheduleException extends RuntimeException {
 
   public ScheduleException() {
-    super(String.format("Please return tomorrow between 9 AM and 18 PM to open your account."));
+    super("Please return tomorrow between 9 AM and 18 PM to open your account.");
   }
 
 }
